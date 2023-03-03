ee0658cf610503b449a0d984ee8f72dc0f9da192
devyoon91 <kimbyungyoun91@gmail.com>
2023-03-03 09:21:18 +0900

    feat: README 추가, 테스트 코드 수정

---
+++ README.md
@@ -0,0 +1,21 @@
+## Whisper Demo
+
+### environment
+- python 3.10.10
+- poetry(패키지 관리용)
+  - pyproject.toml 참조
+- ffmpeg
+    ```
+    # on Windows using Chocolatey (https://chocolatey.org/)
+    choco install ffmpeg
+    ```
+
+
+
+### Whisper models
+- ['tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large']
+
+### Reference
+- https://github.com/openai/whisper
+  - https://github.com/openai/whisper/blob/main/notebooks/Multilingual_ASR.ipynb
+- https://cdn.openai.com/papers/whisper.pdf
--- tests/whisper_use_cpu_transcibe.py
+++ tests/whisper_use_cpu_transcibe.py
@@ -2,19 +2,36 @@ import time
 
 import whisper
 
+# print available model names
+print(whisper.available_models())
+
 # param : model, device[cpu, cuda(default)]
 # model : tiny -> tiny.en -> base -> ...
 # 한국어는 미디엄 이상되야 품질이 나온다고함
-model = whisper.load_model("base", "cpu")
+model = whisper.load_model("small", "cpu")
 
 # FP16 is not supported on CPU; using FP32 instead
 # ../samples/event_horizon.mp3 -> 윤하 - 사건의 지평선
+audio = "../samples/event_horizon.mp3"
+language = "ko"
+
+# options : beam_size ?
+# options : best_of ?
+# options : fp16 ?
+# options = dict(language=language, beam_size=5, best_of=5, fp16=False)
+options = dict(language=language, fp16=False)
+transcribe_options = dict(task="transcribe", **options)
+
 start = time.time()
-result = whisper.transcribe(model=model, audio="../samples/event_horizon.mp3", fp16=False)
+result = model.transcribe(audio, **transcribe_options)
 end = time.time()
 
 # print the recognized text
 print("language : ", result['language'])
-print("segments : ", result['segments'])
 print("text : ", result['text'])
-print(f"{end - start:.5f} sec")
+print(f"executed time {end - start:.5f} sec")
+print("===============================================================================================================")
+
+# print segments
+for segment in result['segments']:
+    print("segments : ", segment)
